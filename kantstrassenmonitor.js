6c1f358280c13a40ef44a7b78816b9911f42e170
Wikinaut <mail@wikinaut.de>
2022-03-17 10:33:25 +0100

    +Kuno-Fischer-Str., +Schlüterstr.

--- kantstrassenmonitor.js
+++ kantstrassenmonitor.js
@@ -44,6 +44,9 @@ const client = createClient('kantstrassenmonitor')
     id: '900000026209',
     name: 'Neue Kantstr./ZOB',
 
+    id: '900000024151',
+    name: 'Kuno-Fischer-Str.',
+-
     id: '900000024104',
     name: 'Amtsgerichtsplatz',
 
@@ -59,6 +62,9 @@ const client = createClient('kantstrassenmonitor')
     id: '900000023105',
     name: 'Uhlandstr./Kantstr.',
 
+    id: '900000024252',
+    name: 'Schlüterstr.',
+
     id: '900000024204',
     name: 'Savignyplatz',
 
@@ -71,11 +77,13 @@ const client = createClient('kantstrassenmonitor')
 var stations = [
     '900000024106',
     '900000026209',
+    '900000024151',
     '900000024104',
     '900000024108',
     '900000024200',
     '900000024207',
     '900000023105',
+    '900000024252',
     '900000024204',
     '900000023206'
 ];
